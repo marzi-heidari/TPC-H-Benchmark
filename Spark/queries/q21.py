06b590c0a39f7400c9990d6f9173b154526c84c9
Marzieh Heidari <shairad060@gmail.com>
2019-01-15 20:36:09 +0330

    avro result completed

--- Spark/queries/q21.py
+++ Spark/queries/q21.py
@@ -1,15 +1,6 @@
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
 
-region=spark.read.parquet("hdfs://namenode:8020/region.parquet")
-part=spark.read.parquet("hdfs://namenode:8020/part.parquet")
-partsupp=spark.read.parquet("hdfs://namenode:8020/partsupp.parquet")
-supplier=spark.read.parquet("hdfs://namenode:8020/supplier.parquet")
-nation=spark.read.parquet("hdfs://namenode:8020/nation.parquet")
-lineitem=spark.read.parquet("hdfs://namenode:8020/lineitem.parquet")
-orders=spark.read.parquet("hdfs://namenode:8020/orders.parquet")
-customer=spark.read.parquet("hdfs://namenode:8020/customer.parquet")
-   
    
 fsupplier = supplier.select("s_suppkey", "s_nationkey", "s_name")
 
